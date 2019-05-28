a08db14c9955699a9ad7dfb40a89387e2d93e937
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-28 16:08:58 -0400

    authRouter added to server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const configureMiddleware = require('./middleware.js');
+const authRouter = require('./auth/authRouter');
 
 const server = express();
-
 configureMiddleware(server);
+
+server.use('/api', authRouter);
 
 server.get("/", async (req, res) => {
   res.status(200).json({ message: "Welcome yo" });
