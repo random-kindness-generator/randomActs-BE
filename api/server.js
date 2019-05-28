ae76189130d69c914873918ed9923aa5906ff8d5
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-28 12:56:31 -0400

    helemt added server set up

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet")
+
+
+const server = express();
+
+server.use(express.json(), helmet(), morgan('dev'));
+
+server.get("/", async (req, res) => {
+  res.status(200).json({ message: "Welcome yo" });
+});
+
+module.exports = server
