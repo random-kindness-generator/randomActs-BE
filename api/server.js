1429b7fab796c26d0ef861d44295a750435720c8
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-28 17:05:45 -0400

    added contacts router to server

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const configureMiddleware = require('./middleware/serverSetup');
 const authRouter = require('./auth/authRouter');
+const contactsRouter = require('./routers/contactsRouter')
 
 const server = express();
 configureMiddleware(server);
 
 server.use('/api', authRouter);
+server.use('/api/contacts', contactsRouter);
 
 server.get("/", async (req, res) => {
   res.status(200).json({ message: "Welcome yo" });
