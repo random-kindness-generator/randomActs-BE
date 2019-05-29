0d5de3a3bec0eb507d0dfd511de276733d73617c
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-29 15:43:11 -0400

    seed tables have data for all tables

--- data/seeds/003-actions.js
+++ data/seeds/003-actions.js
@@ -1,44 +1,14 @@
 exports.seed = function(knex, Promise) {
   return knex("users").insert([
-    {
-      id: 1,
-      action: "pay it forward"
-    },
-    {
-      id: 2,
-      action: "pay it forward"
-    },
-    {
-      id: 3,
-      action: "pay it forward"
-    },
-    {
-      id: 4,
-      action: "pay it forward"
-    },
-    {
-      id: 5,
-      action: "pay it forward"
-    },
-    {
-      id: 6,
-      action: "pay it forward"
-    },
-    {
-      id: 7,
-      action: "pay it forward"
-    },
-    {
-      id: 8,
-      action: "pay it forward"
-    },
-    {
-      id: 9,
-      action: "pay it forward"
-    },
-    {
-      id: 10,
-      action: "pay it forward"
-    }
+    { id: 1, action: "Pay it forward" },
+    { id: 2, action: "Mow their yard" },
+    { id: 3, action: "Take the trash out" },
+    { id: 4, action: "Treat them to breakfast" },
+    { id: 5, action: "Send them flowers" },
+    { id: 6, action: "Tell them to get this app" },
+    { id: 7, action: "Go on a walk with them" },
+    { id: 8, action: "Take them to the Zoo" },
+    { id: 9, action: "Buy them a coffee" },
+    { id: 10, action: "Help them, for crying out loud, just find something and help them with it!" }
   ]);
 };
