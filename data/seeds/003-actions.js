7c62d768a5567a090cfe11c183c1972d04b5ff4b
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-30 20:16:48 -0400

    seed data added

--- data/seeds/003-actions.js
+++ data/seeds/003-actions.js
@@ -9,6 +9,16 @@ exports.seed = function(knex, Promise) {
     { id: 7, action: "Go on a walk with them" },
     { id: 8, action: "Take them to the Zoo" },
     { id: 9, action: "Buy them a coffee" },
-    { id: 10, action: "Help them, for crying out loud, just find something and help them with it!" }
+    { id: 10, action: "Help them, for crying out loud, just find something and help them with it!" },
+    { id: 11, action: "Pay it forward" },
+    { id: 12, action: "Mow their yard" },
+    { id: 13, action: "Take the trash out" },
+    { id: 14, action: "Treat them to breakfast" },
+    { id: 15, action: "Send them flowers" },
+    { id: 16, action: "Tell them to get this app" },
+    { id: 17, action: "Go on a walk with them" },
+    { id: 18, action: "Take them to the Zoo" },
+    { id: 19, action: "Buy them a coffee" },
+    { id: 20, action: "Help them, for crying out loud, just find something and help them with it!" }
   ]);
 };
